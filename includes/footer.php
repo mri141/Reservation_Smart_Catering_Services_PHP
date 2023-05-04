1a6d99c3881c3e6a6d783d82e4d3a5492b3424f7
mri141 <rashedcse35@gmail.com>
2023-05-05 01:55:24 +0600

    Initial Commit by Rashed

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+            <!-- Copyright info -->
+            <p class="copy">Copyright &copy; 2017 - Smart Catering Services| Developed by - <a href="#"> Rashed & Mollik</a> </p>
+      </div>
+    </div>
+  </div>
+</footer> 
---
+++ includes/title.php
@@ -0,0 +1,1 @@
+Smart Catering Services - Reservation
